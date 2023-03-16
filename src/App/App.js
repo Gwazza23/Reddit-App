eb09c7e26f948d7a089c0f849f856cdcd975eb9c
Gwazza23 <weerawarnagayan@gmail.com>
2023-03-16 19:34:42 +0000

    implement react router into the app

--- src/App/App.js
+++ src/App/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
 
+//import react-router-dom 
+import {RouterProvider, createBrowserRouter, createRoutesFromElements, Route} from 'react-router-dom'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+      <Route path="/" />
+    </Route>
+  )
+)
+
 function App() {
-  return <></>;
+  return (
+    <RouterProvider router={router} />
+  );
 }
 
 export default App;
