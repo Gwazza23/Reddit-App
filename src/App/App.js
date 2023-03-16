0ed56da23a5c4834a20f98aedd5115317cd0fd70
Gwazza23 <weerawarnagayan@gmail.com>
2023-03-16 19:51:38 +0000

    create searchbar component + css

--- src/App/App.js
+++ src/App/App.js
@@ -2,11 +2,12 @@ import "./App.css";
 
 //import react-router-dom 
 import {RouterProvider, createBrowserRouter, createRoutesFromElements, Route} from 'react-router-dom'
+import SearchBar from "../Components/SearchBar/SearchBar";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route>
-      <Route path="/" />
+      <Route path="/" element={<SearchBar />}/>
     </Route>
   )
 )
---
+++ src/Components/SearchBar/SearchBar.js
@@ -0,0 +1,12 @@
+export default function SearchBar() {
+  return (
+    <div className="search-bar-div">
+      <h1>
+        Reddit<span>.min</span>
+      </h1>
+      <form>
+        <input />
+      </form>
+    </div>
+  );
+}
